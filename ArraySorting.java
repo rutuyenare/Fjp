e3974a13ded7d188d6441611d26a4397786ccebf
akmane <yenarerutuja88@gmail.com>
2025-11-11 22:49:10 +0530

    Update ArraySorting.java

--- ArraySorting.java
+++ ArraySorting.java
@@ -1,28 +1,20 @@
-package exp5;
+package sorting;
+import java.util.*;
 
-	import java.util.*;
-
-	public class SimpleSort {
+public class Sorting {
+	
 	    public static void main(String[] args) {
-	        Scanner in = new Scanner(System.in);
-	        System.out.println("1. Integers  2. Strings");
-	        int ch = in.nextInt(); in.nextLine();
+	        
+	        int[] numbers = {42, 17, 8, 99, 23};
+	        Arrays.sort(numbers);
+	        System.out.println("Sorted integers: " + Arrays.toString(numbers));
 
-	        System.out.print("How many elements? ");
-	        int n = in.nextInt(); in.nextLine();
-
-	        if (ch == 1) {
-	            int[] a = new int[n];
-	            System.out.println("Enter numbers:");
-	            for (int i = 0; i < n; i++) a[i] = in.nextInt();
-	            Arrays.sort(a);
-	            System.out.println("Sorted: " + Arrays.toString(a));
-	        } else {
-	            String[] s = new String[n];
-	            System.out.println("Enter names:");
-	            for (int i = 0; i < n; i++) s[i] = in.nextLine();
-	            Arrays.sort(s);
-	            System.out.println("Sorted: " + Arrays.toString(s));
-	        }
+	       
+	        String[] names = {"Rutuja", "Amit", "Sneha", "Zara", "Bhavesh"};
+	        Arrays.sort(names);
+	        System.out.println("Sorted names: " + Arrays.toString(names));
 	    }
 	}
+
+
+
