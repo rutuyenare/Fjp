796d6dae0cbd8ed882c7db89e44cb75f4dbb7dd0
akmane <yenarerutuja88@gmail.com>
2025-11-11 17:12:42 +0530

    Implement method overloading in Employee class

---
+++ MethodOverloading.java
@@ -0,0 +1,21 @@
+
+		public class Employee {
+
+		    Employee(int rollNo,String name){
+		        System.out.println(rollNo+" "+name);
+		    }
+		    Employee(int id , int age , String name){
+		        System.out.println(id+" "+age+" "+name);
+		    }
+
+		    public static void main(String[] args) {
+		        Employee e1 = new Employee(19,"Siddhant");
+		        Employee e2 = new Employee(7019,21,"Siddhant");
+
+		    }
+		}
+
+
+	
+	
+
