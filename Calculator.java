7e8d7d84339b180da6703558744b6e182b84a60d
akmane <yenarerutuja88@gmail.com>
2025-11-11 17:20:16 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,62 +1,55 @@
-package calculator;
 import java.util.Scanner;
 
-		public class calculator {
-		    public static void main(String[] args) {
-		        Scanner in = new Scanner(System.in);
-		        System.out.println("Welcome to calculator");
-		       
-		        int a= 15;
-		        System.out.print("Enter 1st number: " +a);
-		        
-		        int b= 50;
-		        System.out.print("Enter 2nd number: " +b);
+public class calculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Welcome to calculator");
+        System.out.print("Enter 1st number: ");
+        int a= in.nextInt();
+        System.out.print("Enter 2nd number: ");
+        int b= in.nextInt();
 
-		        calculator ob = new calculator();
-		        int c;
-		        do {
-		        System.out.println("MENU: 1.Addition  2.Subtraction 3.Multiplication 4.Division 5.Exit");
-		        System.out.println("Enter your choice");
-		         c = in.nextInt();
+        calculator ob = new calculator();
+        int c;
+        do {
+        System.out.println("MENU: 1.Addition  2.Subtraction 3.Multiplication 4.Division 5.Exit");
+        System.out.println("Enter your choice");
+         c = in.nextInt();
 
-		            switch (c){
-		                case 1 :
-		                    System.out.println("Addition is: " + ob.add(a,b));
-		                    break;
-		                case 2 :
-		                    System.out.println("Subtraction is: " + ob.sub(a,b));
-		                    break;
-		                case 3 :
-		                    System.out.println("Multiplication is: " + ob.mul(a,b));
-		                    break;
-		                case 4 :
-		                    System.out.println("Division is: " + ob.div(a,b));
-		                    break;
-		                case 5 :
-		                    System.out.println("Thank you!");
-		                    break;
-		                default :
-		                    System.out.println("invalid choice");
-		            }
-		        }while (c!=5);
+            switch (c){
+                case 1 :
+                    System.out.println("Addition is: " + ob.add(a,b));
+                    break;
+                case 2 :
+                    System.out.println("Subtraction is: " + ob.sub(a,b));
+                    break;
+                case 3 :
+                    System.out.println("Multiplication is: " + ob.mul(a,b));
+                    break;
+                case 4 :
+                    System.out.println("Division is: " + ob.div(a,b));
+                    break;
+                case 5 :
+                    System.out.println("Thank you!");
+                    break;
+                default :
+                    System.out.println("invalid choice");
+            }
+        }while (c!=5);
 
 
-		    }
+    }
 
-		    int add(int a,int b){
-		        return a+b;
-		    }
-		    int sub(int a,int b){
-		        return a-b;
-		    }
-		    int mul(int a,int b){
-		        return a*b;
-		    }
-		     int div(int a,int b){
-		        return a%b;
-		    }
-		
-
-	}
-
-
+    int add(int a,int b){
+        return a+b;
+    }
+    int sub(int a,int b){
+        return a-b;
+    }
+    int mul(int a,int b){
+        return a*b;
+    }
+     int div(int a,int b){
+        return a%b;
+    }
+}
