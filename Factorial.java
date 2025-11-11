ea2cd87a72a220f9d23ba2da019b6ebbec126ccf
akmane <yenarerutuja88@gmail.com>
2025-11-11 17:19:48 +0530

    Refactor Factorial class for user input

--- Factorial.java
+++ Factorial.java
@@ -1,34 +1,29 @@
 import java.util.Scanner;
 
-		public class factorial {
+public class factorial {
 
-		    public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		        Scanner sc = new Scanner(System.in);
-		        System.out.println("Welcome to factorial calculator");
-		        
-		        int num = 5;
-		        System.out.print("Enter the number: " +num);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Welcome to factorial calculator");
+        System.out.print("Enter the number: ");
+        int num = in.nextInt();
 
-		        factorial ob = new factorial();
-		        int factorial = ob.fact(num);
+        factorial ob = new factorial();
+        int factorial = ob.fact(num);
 
-		        System.out.println("factorial of "+ num+" is "+factorial);
+        System.out.println("factorial of "+ num+" is "+factorial);
 
-		    }
+    }
 
-		    int fact(int num){
+    int fact(int num){
 
-		        int fact = 1;
-		        int i=num;
-		        while(i>0){
-		            fact = fact * i;
-		            i--;
-		        }
-		        return fact;
-		    }
-		
-
-	}
-
-
+        int fact = 1;
+        int i=num;
+        while(i>0){
+            fact = fact * i;
+            i--;
+        }
+        return fact;
+    }
+}
